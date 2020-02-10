656ab30605600790029a8a695ee55cacce1b4251
Nelya Parfenova <nelya.parfenova@gmail.com>
2020-02-10 23:27:55 +0300

    app done

--- geo_back/settings.py
+++ geo_back/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # installed
+    'rest_framework',
+    # apps
+    'api',
+
 ]
 
 MIDDLEWARE = [
