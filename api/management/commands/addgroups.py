9261ff5775e8ef60a386af692efa88046da2dde3
Nelya Parfenova <nelya.parfenova@gmail.com>
2020-02-23 21:52:41 +0300

    fix

---
+++ api/management/commands/addgroups.py
@@ -0,0 +1,33 @@
+import logging
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
+
+GROUPS = ['GUEST', 'SALES', 'REPORTER', ]
+MODELS = [
+    'user', 
+    'report', 
+    'order', 
+    ]
+PERMISSIONS = ['view', 'add', 'change', ] 
+
+class Command(BaseCommand):
+    help = "Creates default group and permissions"
+
+    def handle(self, *args, **options):
+        for group in GROUPS:
+            new_group, created = Group.objects.get_or_create(name=group)
+            for model in MODELS:
+                for permission in PERMISSIONS:
+                    codename = '{}_{}'.format(permission, model)
+                    try:
+                        model_add_perm = Permission.objects.get(codename=codename)
+                    except Permission.DoesNotExist:
+                        logging.warning("Permission not found with name '{}'.".format(name))
+                        continue
+
+                    new_group.permissions.add(model_add_perm)        
+        self.stdout.write(self.style.SUCCESS("Created default groups and permissions."))
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py addgroups
 python manage.py createsuperu --username=admin --password=pwd0123456789
-
 python manage.py runserver 0.0.0.0:8000
