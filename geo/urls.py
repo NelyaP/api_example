c7cbd3ff7b130713c233645f5f56e7e5743682cf
Nelya Parfenova <nelya.parfenova@gmail.com>
2020-04-09 21:48:53 +0300

    register url

--- geo/urls.py
+++ geo/urls.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from api.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +12,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('api/register/', register),
 ]
