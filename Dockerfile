ad9c1cc1046d627c507178e8ba7c731b5cee86d1
Nelya Parfenova <nelya.parfenova@gmail.com>
2020-10-02 14:14:15 +0300

    cities

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM python:3.6-slim
+FROM python:3.8.5-slim
 ENV PYTHONUNBUFFERED 1
 COPY . /code
 WORKDIR /code
+RUN ls -la
 RUN chmod +x entrypoint.sh
+#RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
-ENTRYPOINT ["./entrypoint.sh"]
+ENTRYPOINT ["/code/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,5 @@ python manage.py addgroups
 python manage.py createsuperu --username=${SUPER_USERNAME} --password=${SUPER_PASS}
 python manage.py loaddata ordstatuses.json
 python manage.py loaddata ordtypes.json
+python manage.py loaddata cities.json
 python manage.py runserver 0.0.0.0:8000
