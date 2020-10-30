f99b01e421ccfde84264e01d31ba534f36e30cd3
Nelya Parfenova <nelya.parfenova@gmail.com>
2020-10-30 09:37:43 +0100

    remove pass from serializer

--- api/serializers.py
+++ api/serializers.py
@@ -36,18 +36,17 @@ class CitySerializer(serializers.ModelSerializer):
 
 
 class AccountSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True,
-        required=True,
-        help_text='Leave empty if no change needed',
-        style={'input_type': 'password', 'placeholder': 'Password'}
-    )
+    #password = serializers.CharField(
+    #    write_only=True,
+    #    required=True,
+    #    help_text='Leave empty if no change needed',
+    #    style={'input_type': 'password', 'placeholder': 'Password'}
+    #)
 
     class Meta:        
         model = Account        
         fields = (
             'username',
-            'password',
             'phone',
             'email',
             'company',
